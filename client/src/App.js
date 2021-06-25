935a9036695ad3f4b9086caa788095e739c49432
Nate <n8blake@mac.com>
2021-06-25 14:39:01 -0700

    Add tint and shade mixing functions to theme-colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 import "./App.scss";
 
@@ -7,6 +9,11 @@ function App() {
   return (
     <div >
       <Header />
+      <Router>
+      <Nav>
+      
+      </Nav>
+      </Router>
     </div>
   );
 }
