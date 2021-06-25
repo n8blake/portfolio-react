79b398c0c7d79b5c4f4bb372cfdcbfab939b424e
Nate <n8blake@mac.com>
2021-06-25 14:04:41 -0700

    Bring over some old styles from original site, add Header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./header.scss";
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1 className="name-hero">n8blake</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
