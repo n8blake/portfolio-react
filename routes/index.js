afa8b27eb05dec2f0072fdc36d4a75f33049d841
Nate <n8blake@mac.com>
2021-06-26 14:54:10 -0700

    Add image resource routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const imageStoreRoutes = require("./images");
 
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
+// IMAGE STORE Routes
+router.use("/images", imageStoreRoutes);
+
+// If no API or IMAGE STORE routes are hit, send the React app
 router.use(function(req, res) {
 if (process.env.NODE_ENV === "production") {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
