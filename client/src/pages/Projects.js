bc95b4c621ca118db8674cf77db04ae766b3886f
Nate <n8blake@mac.com>
2021-06-26 14:55:04 -0700

    Get project data from API and populate list

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,12 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import API from "../utils/API";
+import "./Projects.scss";
 
 function Projects(){
 
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        loadProjects();
+    }, [])
+
+    function loadProjects() {
+        API.getProjects()
+            .then(response => {
+                console.log(response);
+                setProjects(response);
+            })
+            .catch(error => {
+                setProjects([])
+                console.error(error)
+            })
+    }
+
     return(
-        <div>
-            <p>Projects page.</p>
-        </div>
+        <section className="section active" >
+            
+                { projects.length ? (
+                    <ul id="portfolio-items-list">
+                    {projects.map(project => {
+                        return (
+                            <li key={project._id} className="portfolio-item">
+                                <a href="project.link">
+                                    <img src={"/images/" + project.image} />
+                                </a>
+                                <div className="item-text">
+                                    <h2 className="item-title">{project.name}</h2>
+                                    <p className={"item-description"}>{project.description}</p>
+                                </div>
+                            </li>
+                        )
+                    })}
+                   </ul>
+                ) : (
+                    <h3>No Projects</h3>
+                )}
+            
+        </section>
     )
 
 }
